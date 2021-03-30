743bc143d6fa31b808585dbfde7a092c7962bbe6
Sean Quillen <sean.r.quillen@gmail.com>
2021-03-30 10:16:44 -0500

    improve no-restaurants screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -48,6 +48,7 @@ export default function Home() {
         <Loading>loading...</Loading>
       ) : (
         <>
+          {/* TOGGLE SECTION */}
           <div
             className={
               atTop ? 'selections-container--white' : 'selections-container'
@@ -55,8 +56,8 @@ export default function Home() {
           >
             <Toggle
               toggle={showSelections}
-              showLabel="Show filter"
-              hideLabel="Hide filter"
+              showLabel="Show filters"
+              hideLabel="Hide filters"
               onClick={() => setShowSelections(!showSelections)}
             />
             <div
@@ -70,69 +71,75 @@ export default function Home() {
               />
             </div>
           </div>
-          <div
-            className="restaurants-section"
-            onClick={showSelections ? () => setShowSelections(false) : null}
-          >
-            <div className="restaurant-detail">
-              {/* MAP SECTION */}
-              <div className="map-section">
-                <div className="left-side">
-                  {selectedRestaurant && (
-                    <div className="map">
-                      <Map
-                        data={restaurantData}
-                        onClick={setSelectedRestaurant}
-                        selectedRestaurant={selectedRestaurant}
-                      />
-                    </div>
-                  )}
+          {restaurantData.length ? (
+            <div
+              className="restaurants-section"
+              onClick={showSelections ? () => setShowSelections(false) : null}
+            >
+              <div className="restaurant-detail">
+                {/* MAP SECTION */}
+                <div className="map-section">
+                  <div className="left-side">
+                    {selectedRestaurant && (
+                      <div className="map">
+                        <Map
+                          data={restaurantData}
+                          onClick={setSelectedRestaurant}
+                          selectedRestaurant={selectedRestaurant}
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="right-side">
+                    {selectedRestaurant ? (
+                      <div className="hero-container">
+                        <div
+                          onClick={() =>
+                            window.open(selectedRestaurant.url, '_blank')
+                          }
+                          className="image-div"
+                          style={{
+                            background: `url(${selectedRestaurant.image}) center no-repeat`,
+                            backgroundSize: 'cover',
+                          }}
+                        />
+                        <RestaurantDetails restaurant={selectedRestaurant} />
+                      </div>
+                    ) : (
+                      <div className="no-results">no restaurant selected</div>
+                    )}
+                  </div>
                 </div>
-                <div className="right-side">
-                  {selectedRestaurant ? (
-                    <div className="hero-container">
+                <div className="restaurants-container">
+                  <h2 className="restaurants-container__header">
+                    {`${restaurantData.length} Restaurants:`}
+                  </h2>
+                  <div className="restaurants-container__restaurants">
+                    {restaurantData.map((restaurant) => (
                       <div
-                        onClick={() =>
-                          window.open(selectedRestaurant.url, '_blank')
-                        }
-                        className="image-div"
-                        style={{
-                          background: `url(${selectedRestaurant.image}) center no-repeat`,
-                          backgroundSize: 'cover',
-                        }}
-                      />
-                      <RestaurantDetails restaurant={selectedRestaurant} />
-                    </div>
-                  ) : (
-                    <div className="no-results">no results!</div>
-                  )}
-                </div>
-              </div>
-              <div className="restaurants-container">
-                <h2 className="restaurants-container__header">
-                  {`${restaurantData.length} Restaurants:`}
-                </h2>
-                <div className="restaurants-container__restaurants">
-                  {restaurantData.map((restaurant) => (
-                    <div
-                      key={restaurant.id}
-                      className="restaurants-container__restaurants--restaurant"
-                    >
-                      <div
-                        className="restaurant-image-div"
-                        onClick={() => window.open(restaurant.url, '_blank')}
-                        style={{
-                          background: `url(${restaurant.image}) center no-repeat`,
-                          backgroundSize: 'cover',
-                        }}
-                      />
-                      <RestaurantDetails restaurant={restaurant} />
-                    </div>
-                  ))}
+                        key={restaurant.id}
+                        className="restaurants-container__restaurants--restaurant"
+                      >
+                        <div
+                          className="restaurant-image-div"
+                          onClick={() => window.open(restaurant.url, '_blank')}
+                          style={{
+                            background: `url(${restaurant.image}) center no-repeat`,
+                            backgroundSize: 'cover',
+                          }}
+                        />
+                        <RestaurantDetails restaurant={restaurant} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div className="no-restaurants">
+              No restaurants match your filters.
+            </div>
+          )}
         </>
       )}
     </div>
