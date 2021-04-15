da395075a2f5dd4a84685db6fb51e5f43ca8aeed
Sean Quillen <sean.r.quillen@gmail.com>
2021-04-15 14:38:56 -0500

    add lazy load

--- src/components/BackgroundImageDiv.js
+++ src/components/BackgroundImageDiv.js
@@ -1,49 +1,57 @@
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import LazyLoad from 'react-lazyload';
 import PropTypes from 'prop-types';
 
 export default function BackgroundImageDiv({ images, styles }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const noOfImages = images && images.length - 1;
+  const noOfImages = images && images.length;
   if (!noOfImages) return null;
   const handleClick = () => (direction) => {
     if (direction < 0) {
       if (currentIndex === 0) setCurrentIndex(noOfImages);
       else setCurrentIndex(currentIndex - 1);
-    } else if (currentIndex === noOfImages) setCurrentIndex(0);
+    } else if (currentIndex === noOfImages - 1) setCurrentIndex(0);
     else setCurrentIndex(currentIndex + 1);
   };
   return (
     <div className='background-image-container'>
-      <div
-        className='background-image--image'
-        style={{
-          backgroundImage: `url(${images[currentIndex].img})`,
-          ...styles,
-        }}
+      <LazyLoad
+        height='100%'
+        once
+        style={{ height: '100%', color: 'black' }}
+        placeholder={<div className='loading'>loading...</div>}
       >
-        {noOfImages >= 1 && (
-          <>
-            <div className='image-toggle-container' onClick={handleClick(-1)}>
-              <img
-                className='image-toggle'
-                src='./svgs/chevron-left.svg'
-                alt='previous'
-              />
-            </div>
-            <div className='image-toggle-container' onClick={handleClick(1)}>
-              <img
-                className='image-toggle'
-                src='./svgs/chevron-right.svg'
-                alt='next'
-              />
-            </div>
-          </>
-        )}
-      </div>
-      <div className='title'>
-        <ReactMarkdown>{images[currentIndex].title}</ReactMarkdown>
-      </div>
+        <div
+          className='background-image--image'
+          style={{
+            backgroundImage: `url(${images[currentIndex].img})`,
+            ...styles,
+          }}
+        >
+          {noOfImages >= 1 && (
+            <>
+              <div className='image-toggle-container' onClick={handleClick(-1)}>
+                <img
+                  className='image-toggle'
+                  src='./svgs/chevron-left.svg'
+                  alt='previous'
+                />
+              </div>
+              <div className='image-toggle-container' onClick={handleClick(1)}>
+                <img
+                  className='image-toggle'
+                  src='./svgs/chevron-right.svg'
+                  alt='next'
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className='title'>
+          <ReactMarkdown>{images[currentIndex].title}</ReactMarkdown>
+        </div>
+      </LazyLoad>
     </div>
   );
 }
